091f3768c4d69f9bc62c6f7686d02173291d5191
David Fleury <dfleury2@free.Fr>
2022-03-03 21:56:47 +0100

    Change conanfile.py export_sources (to be checked)

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class BeautyConan(ConanFile):
     requires        = ("boost/1.77.0@",
                        "openssl/1.1.1g@")
 
-    exports_sources = "*", "!build"
+    exports_sources = "cmake", "examples*", "include*", "src*", "CMakeLists.txt", "conanfile.py"
 
     def configure_cmake(self):
         cmake = CMake(self)
