84349ac3da9acda68c8ac57813ae25f838ad5f77
David Fleury <dfleury2@free.fr>
2024-01-25 21:18:55 +0100

    Set version to 1.0.3

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 class BeautyConan(ConanFile):
     name            = "beauty"
     description     = "HTTP Server above Boost.Beast"
-    version         = "1.0.2"
+    version         = "1.0.3"
     url             = "https://github.com/dfleury2/beauty"
     license         = "MIT"
     settings        = "os", "compiler", "build_type", "arch"
